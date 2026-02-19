0bdda67ba9c1316e70c42fb8cd7834a976142bb8
Alexei <a_subscriber@mail.ru>
2026-02-19 21:50:33 +0200

    feat: Задача 1 - Аналитика экранов мобильного приложения

---
+++ src/main/java/com/gmail/alexei28/shortcutkafkaconsumer/task1/repo/Task1Repository.java
@@ -0,0 +1,9 @@
+package com.gmail.alexei28.shortcutkafkaconsumer.task1.repo;
+
+import com.gmail.alexei28.shortcutkafkaconsumer.task1.entity.Task1;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Task1Repository extends JpaRepository<Task1, Long> {
+  Optional<Task1> findByNumber(Long number);
+}
